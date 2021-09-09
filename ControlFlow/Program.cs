64358844fdd636c3f764111caef0cb9648346558
Opilane TTHK <opilane@tthk.ee>
2021-09-09 10:32:23 +0300

    Add control Flow

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis on Teie nimi?");
+
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine($"Palun sisestage oma vanus {FirstName}");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana.");
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("Oled liiga noor.");
+
+            }
+            else
+            {
+                Console.WriteLine("Oledki 18.");
+            }
+
+
+
+            //programm küsib kasutaja vanust
+            //programm kasutaja on vanem ku i18, siis kuvatakse "sa oled piisavalt vana"
+            // kui kasutaja on nooorem ki 18 siis konsool kuvab et "oled liiga noor"
+            //kui kasutaja on 18 siis Kuvatakse "oledki 18 "
+
+        }
+    }
+}
---
+++ WhenWereYouBorn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhenWereYouBorn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis on Teie nimi?");
+
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine($"Tere, mis on teie sünni aasta {FirstName}?");
+
+            int SünniAasta = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int Age = 2021 - SünniAasta;
+
+            Console.WriteLine("Teie vanus on " + Age);
+
+
+
+            //programm kysib kasutaja sünni aasta
+            //Progtamm arvutab kasutaja vanustr
+            //programm kuvabkasutaja vanust
+        }
+    }
+}
